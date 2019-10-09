d68eff37cad5ba2f2490139649aed6d378f22e01
tkrebes <39509337+tkrebes@users.noreply.github.com>
2019-10-09 01:08:27 -0500

    Added __slots__ to property classes (#17)
    * Added __slots__ to property classes
    
    * Removed __slots__ from IndexedPropertyItems

--- nisyscfg/xnet/__init__.py
+++ nisyscfg/xnet/__init__.py
@@ -1,4 +1,4 @@
 from nisyscfg.xnet.properties import Filter as FilterProperties  # noqa: F401
 from nisyscfg.xnet.properties import Resource as ResourceProperties  # noqa: F401
 
-from nisyscfg.xnet.enums import enums  # noqa: F401
+from nisyscfg.xnet import enums  # noqa: F401
