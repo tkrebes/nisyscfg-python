f1d1e0a83d41e0cd014aab098e0da533f99b3b11
Tyler Krehbiel <tyler.krehbiel@ni.com>
2020-09-30 10:41:20 -0500

    Moved Filter class to its own file

---
+++ nisyscfg/filter.py
@@ -0,0 +1,37 @@
+import ctypes
+import nisyscfg.errors
+import nisyscfg.properties
+import nisyscfg.xnet.properties
+
+from nisyscfg._lib import c_string_encode
+
+
+@nisyscfg.properties.PropertyBag(nisyscfg.properties.Filter)
+@nisyscfg.properties.PropertyBag(nisyscfg.xnet.properties.Filter, expert='xnet')
+class Filter(object):
+    def __init__(self, session):
+        self._handle = nisyscfg.types.FilterHandle()
+        self._library = nisyscfg._library_singleton.get()
+        self._property_accessor = nisyscfg.properties.PropertyAccessor(setter=self._set_property_with_type)
+        error_code = self._library.CreateFilter(session, ctypes.pointer(self._handle))
+        nisyscfg.errors.handle_error(self, error_code)
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self._handle:
+            error_code = self._library.CloseHandle(self._handle)
+            nisyscfg.errors.handle_error(self, error_code)
+            self._handle = None
+
+    def _set_property_with_type(self, id, value, c_type, nisyscfg_type):
+        if c_type == ctypes.c_char_p:
+            value = c_string_encode(value)
+        elif issubclass(c_type, nisyscfg.enums.BaseEnum):
+            value = ctypes.c_int(value)
+        else:
+            value = c_type(value)
+
+        error_code = self._library.SetFilterPropertyWithType(self._handle, id, nisyscfg_type, value)
+        nisyscfg.errors.handle_error(self, error_code)
--- nisyscfg/session.py
+++ nisyscfg/session.py
@@ -3,6 +3,7 @@ import ctypes
 
 import nisyscfg
 import nisyscfg._library_singleton
+import nisyscfg.filter
 import nisyscfg.properties
 import nisyscfg.pxi.properties
 import nisyscfg.xnet.properties
@@ -193,7 +194,7 @@ class Session(object):
         Raises an nisyscfg.errors.LibraryError exception in the event of an
         error.
         """
-        filter = Filter(self._session)
+        filter = nisyscfg.filter.Filter(self._session)
         self._children.append(filter)
         return filter
 
@@ -635,37 +636,6 @@ class ExpertInfoIterator(object):
             error_code = self._library.CloseHandle(self._handle)
             nisyscfg.errors.handle_error(self, error_code)
             self._handle = None
-
-
-@nisyscfg.properties.PropertyBag(nisyscfg.properties.Filter)
-@nisyscfg.properties.PropertyBag(nisyscfg.xnet.properties.Filter, expert='xnet')
-class Filter(object):
-    def __init__(self, session):
-        self._handle = nisyscfg.types.FilterHandle()
-        self._library = nisyscfg._library_singleton.get()
-        self._property_accessor = nisyscfg.properties.PropertyAccessor(setter=self._set_property_with_type)
-        error_code = self._library.CreateFilter(session, ctypes.pointer(self._handle))
-        nisyscfg.errors.handle_error(self, error_code)
-
-    def __del__(self):
-        self.close()
-
-    def close(self):
-        if self._handle:
-            error_code = self._library.CloseHandle(self._handle)
-            nisyscfg.errors.handle_error(self, error_code)
-            self._handle = None
-
-    def _set_property_with_type(self, id, value, c_type, nisyscfg_type):
-        if c_type == ctypes.c_char_p:
-            value = c_string_encode(value)
-        elif issubclass(c_type, nisyscfg.enums.BaseEnum):
-            value = ctypes.c_int(value)
-        else:
-            value = c_type(value)
-
-        error_code = self._library.SetFilterPropertyWithType(self._handle, id, nisyscfg_type, value)
-        nisyscfg.errors.handle_error(self, error_code)
 
 
 class HardwareResourceIterator(object):
