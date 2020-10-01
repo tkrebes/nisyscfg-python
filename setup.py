f1c606354fdfb7c7f7ecaacd6f5bd50ee863ef28
Tyler Krehbiel <tyler.krehbiel@ni.com>
2020-10-01 12:31:04 -0500

    Removed Python 3.5 support

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
