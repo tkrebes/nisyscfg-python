bd469718a509ddf96129595cf27df30972cc1d1d
Tyler Krehbiel <tyler.krehbiel@ni.com>
2020-12-14 01:51:04 -0600

    Made example compatible with Python 3.5

--- examples/print_temperatures.py
+++ examples/print_temperatures.py
@@ -7,19 +7,19 @@ def print_resource_temperature(resource, indent):
     for name, reading in itertools.zip_longest(
         resource.temperature_name, resource.temperature_reading
     ):
-        print(f"{indent}{name or 'Temperature'}: {reading}")
+        print(indent + "{}: {}".format(name or "Temperature", reading))
 
 
 def print_device_temperatures(session, provides_link_name, indent=""):
-    print(f"{indent}Devices:")
+    print(indent + "Devices:")
     filter = session.create_filter()
     filter.is_present = True
     filter.is_ni_product = True
     filter.is_device = True
     filter.connects_to_link_name = provides_link_name
     for device in session.find_hardware(filter):
-        print(f"{indent}    {device.product_name}: {device.name}")
-        print_resource_temperature(device, f"{indent}    ")
+        print(indent + "    {}: {}".format(device.product_name, device.name))
+        print_resource_temperature(device, indent + "        ")
 
 
 def print_chassis_temperatures(session):
@@ -29,7 +29,7 @@ def print_chassis_temperatures(session):
     filter.is_ni_product = True
     filter.is_chassis = True
     for chassis in session.find_hardware(filter):
-        print(f"    {chassis.name}")
+        print("    " + chassis.name)
         print_resource_temperature(chassis, indent="        ")
         print_device_temperatures(
             session, chassis.provides_link_name, indent="        "
